081cf2ffa9d3ed6024e7a9991271c5d761e64aba
Dzejkop <jakubtrad@gmail.com>
2017-05-07 18:10:09 +0200

    Utility script - Sequencer.

---
+++ Assets/Scripts/Sequencer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Sequencer : MonoBehaviour {
+	public float m_Delay;
+	public UnityEvent m_OnTimeout;
+	private float _t = 0;
+
+	void Update () 
+	{
+		_t += Time.deltaTime;
+
+		if (_t >= m_Delay)
+		{
+			_t = 0;
+			m_OnTimeout.Invoke();
+		}
+	}
+}
